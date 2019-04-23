7841626985b0a46cc2fa5145594de5dd36e043f0
Igor Camões <itscamoes@gmail.com>
2019-04-23 11:53:01 -0300

    Adicionar exercícios

--- teste.py
+++ teste.py
@@ -109,29 +109,74 @@
 # else:
 #         print('Puts que pena, não existe nenhum celular disponível para você comprar no momento')
 
-nomes = [1, 2, 3, 3, 4, 5, 6, 7]
-# print(nomes[1:3])
 
-# nomes.append(20)        #Adiciona 20 ao final da lista
+#         Listas
+# nomes = [1, 2, 3, 3, 4, 4, 4, 5, 6, 7]
+# # print(nomes[1:3])
+
+# # nomes.append(20)        #Adiciona 20 ao final da lista
+# # print(nomes)
+# # nomes.insert(0, 99)     #Adiciona 99 no índice 0
+# # print(nomes)
+# # nomes.remove(3)         #Remove o item de valor 3 na lista
+# # print(nomes)
+# # nomes.pop()             #Remove o último item da lista
+# # print(nomes)
+# # print(nomes.index(4))   #Verifica em que índice o item está na lista
+
+# # print(nomes.count(7))   #Conta quantos itens com esse valor existem na lista
+
+# # lista2 = nomes.copy()   #Faz uma cópia da lista especificada
+# # print(lista2)
+# # nomes.clear()           #Limpa a lista
+# # print(nomes)
+#         Criar uma cópia e eliminar os nomes repetidos
+# nomes2 = []
+
+# for number in nomes:
+#     if number not in nomes2:            #Verifica se o nome não está no nomes2
+#         nomes2.append(number)           #Insere o nome na lista nomes2
 # print(nomes)
-# nomes.insert(0, 99)     #Adiciona 99 no índice 0
-# print(nomes)
-# nomes.remove(3)         #Remove o item de valor 3 na lista
-# print(nomes)
-# nomes.pop()             #Remove o último item da lista
-# print(nomes)
-# print(nomes.index(4))   #Verifica em que índice o item está na lista
+# print(nomes2)
 
-# print(nomes.count(7))   #Conta quantos itens com esse valor existem na lista
+#            Tuplas ou tuple(listas ineditáveis)
 
-# lista2 = nomes.copy()   #Faz uma cópia da lista especificada
-# print(lista2)
-# nomes.clear()           #Limpa a lista
-# print(nomes)
+# cursos = ('Matemática', 'Inglês', 'Biologia')
 
-nomes2 = nomes.copy()
+#            Dicionarios
 
-nomes2.remove(3)
+# pessoa = {
+#     'name': 'Hayane',
+#     'age': 22,
+#     'address': 'área de preservação florestal',
+#     'skills': ['python', 'c', 'cobol', 'pascal', 'pearl', 'birl']
+# }
 
-print(nomes)
-print(nomes2)
+# print(pessoa['name'])
+# print(pessoa['address'])
+# print(pessoa['skills'[0]])
+
+#faça um codigo para printar o número mais alto da lista
+
+# lista = [1, 2, 3, 8, 10, 45, 12]
+
+# print(max(lista))
+# max = lista[0]
+
+# for lista in lista:
+#     if lista > max:
+#         max = lista
+# print(max)
+
+
+#faça um codigo, onde é possível identificar se o aluno passou na faculdade ou não. A média é 6, e ele só pode ter 2 faltas.
+
+# media = int(input("Qual a sua média?"))
+# faltas = int(input("Quantas faltas você teve?"))
+
+# if media > 6 and faltas < 2:
+#     print("Parabéns, você passou na faculdade.")
+# elif media > 6 and faltas > 2:
+#     print("Reprovado por conta de faltas.")
+# elif media < 6:
+#     print("Reprovado por conta de média.")
