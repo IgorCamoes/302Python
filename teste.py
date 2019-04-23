0694485a8ac39d70e167cf17f864a8b92a2a7c66
Igor Camões <itscamoes@gmail.com>
2019-04-23 09:53:39 -0300

    Criar algoritmo em python3

---
+++ teste.py
@@ -0,0 +1,49 @@
+# full_name = "Ana Beatriz"
+# birth_year = input("Em que ano você nasceu?")
+# year = 2019
+# print(year - int(birth_year))
+
+
+#     conversor de libras pra kilos
+# libras = input('Qual o peso em libras?')
+# conversor = 0.45
+# kg = int(libras) * float(conversor)
+
+# print(float(kg))
+
+
+#     OTIMIZADO
+# peso_lbs = input('Descubra quanto vale em kg')
+# peso_kg = int(peso_lbs) * 0.45
+
+# print(peso_kg)
+
+#       concatenação de strings
+# name = input("Qual o seu nome?")
+# color = input("Qual a sua cor favorita? ")
+
+# print("Você se chama " + name + " e gosta da cor " +color)
+
+
+#         Acessando indices dentro da variavel que não é especificamente um array
+# name = "Ana Beatriz"
+# print(name[1:5])
+# print(len(name))
+# print(name.find("A"))
+# print(name.replace("Beatriz", "Morita"))
+
+#         Procurar um valor na variável inteira
+# course = "Não sei o que colocar então vou ficar digitando pra caramba até eu conseguir muito texto"
+# print("caramba" in course)
+
+#         Condições
+# summer = True
+# winter = False
+
+# if summer:
+#     print("Use óculos de sol")
+# elif winter:
+#     print("Não esquece o casaco, filho")
+# else: 
+#     print("Não use óculos de sol")
+
